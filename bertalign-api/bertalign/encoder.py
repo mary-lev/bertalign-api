8e3f1e8c7e05b3bacecabd9990f5e4a613511ecf
marylev <marylevchenko@gmail.com>
2025-07-15 00:58:27 +0200

    Rebuild HF models

--- bertalign-api/bertalign/encoder.py
+++ bertalign-api/bertalign/encoder.py
@@ -5,7 +5,7 @@ from bertalign.utils import yield_overlaps
 
 class Encoder:
     def __init__(self, model_name):
-        self.model = SentenceTransformer(model_name)
+        self.model = SentenceTransformer(f"/app/{model_name}")
         self.model_name = model_name
 
     def transform(self, sents, num_overlaps):
